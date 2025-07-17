a8b7ce5fa8b0addda2a609c6777b3e7d0674f5bd
nafa7Code <nafa7gpt@gmail.com>
2025-07-17 22:47:33 +0300

    Ignore .env, add python-dateutil requirement & static-serve fix

--- logistics_project/urls.py
+++ logistics_project/urls.py
@@ -31,7 +31,11 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
 # Custom error handlers
 handler404 = handler404
 handler500 = handler500
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
