9ff60e4d7ba0761abf15682600540f449b75c67f
nafa7Code <nafa7gpt@gmail.com>
2025-07-17 14:22:44 +0300

    Fix build script: use python3 -m pip for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# 1) تحديث pip عبر Python
 python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade pip
-pip install -r requirements.txt
 
+# 2) تثبيت المتطلبات
+python3 -m pip install -r requirements.txt
+
+# 3) مايجريشنز
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
+
+# 4) جمع الملفات الثابتة
 python3 manage.py collectstatic --noinput --clear
